b04e0fd14ed623e4b34d6adf18e33e3e80d4e040
2024125073 <anjiwon995@gmail.com>
2026-06-01 15:35:44 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -4,27 +4,26 @@ int main(void) {
     int n;
     int arr1[20] = {};
     int arr2[20] = {};
+    int *p1 = nullptr , *p2 = nullptr ; 
+    
 
     scanf("%d", &n);
 
-    for (int i = 0; i < n; i++) { // 첫번째 배열 원소 입력
-        scanf("%d", &arr1[i]);
+    for (p1 = arr1; p1 < arr1 + n; p1++) { // 첫번째 배열 원소 입력
+        scanf("%d", p1);
     }
 
-    for (int i = 0; i < n; i++) { // 두번째 배열 원소 입력
-        scanf("%d", &arr2[i]);
+    for (p2 = arr2; p2 < arr2 + n; p2++) { // 두번째 배열 원소 입력
+        scanf("%d", p2);
     }
 
 
-    int *p1 = arr1; // 앞에서부터 (배열의 시작점)
-    int *p2 = arr2 + (n - 1); // 뒤에서부터 (배열의 마지막)
+    p1 = arr1; // 앞에서부터 (배열의 시작점)
+    p2 = arr2 + (n - 1); // 뒤에서부터 (배열의 마지막)
 
 
-    for (int i = 0; i < n; i++) { // 역방향으로 더하기
+    for (p1 = arr1; p1 < arr1 + n; p1++,p2--) { // 역방향으로 더하기
         printf(" %d", *p1 + *p2); // 공백이 먼저 출력 => " %d"
-        
-        p1++; 
-        p2--; 
     }
     printf("\n");
 
